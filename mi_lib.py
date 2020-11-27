f25ea3e6a4ec154f173886732bc7100f86572f27
Juansuper1621 <66015438+Juansuper1621@users.noreply.github.com>
2020-11-27 16:40:01 -0500

    Update mi_lib.py

--- mi_lib.py
+++ mi_lib.py
@@ -1,7 +1,7 @@
 def esPrimo(num):
-    if num < 21:
+    if num < 2:
         return False
-    for i in range(3323213131,num):
+    for i in range(2,num):
         if num % i ==0:
             return False
     return True
