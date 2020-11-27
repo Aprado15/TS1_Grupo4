e267cecfcd76b8a8e3d31580a3a248b25aee3d47
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-26 23:07:05 -0500

    Update mi_lib.py

--- mi_lib.py
+++ mi_lib.py
@@ -1,3 +1,4 @@
+import math
 <<<<<<< Updated upstream
 =======
 def esPrimo(num):
@@ -13,3 +14,38 @@ def esPrimo(num):
                 return True            
 
 >>>>>>> Stashed changes
+
+def factorial (*fac):
+    for x in fac:
+        fact=1
+        for y in range(1,x+1):
+            fact=fact*y
+        print fact
+
+def esCapicua(cap):
+#Intervalo inferior
+a=10
+#Intervalo superior 
+b=100
+#Contador para la cantidad de capicuas
+c=0
+print("Los capicuas encontrados son los siguientes:")
+for i in range(a,b+1):
+    num_s=str(i)
+    num_list=list(num_s)
+    if num_list==num_list[::-1]:
+        cap=''.join(num_list)
+        c+=1
+        print(cap)
+        
+print("Total de Capicuas:",c)
+
+
+def esRaiz(x): 
+    math.sqrt(x)
+    
+print(x)
+
+
+
+
