e6c08e5014a8473e330f8724715840a91ff58a6e
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-27 01:31:58 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,7 +1,7 @@
 import mi_lib
 print(f"Si sale true El número ingresado es primo y si sale false no es primo",mi_lib.esPrimo(7))
 
-print(f"El factorial del número ingresado es {mi_lib.esFactorial(5)}")
+print(f"El factorial del número ingresado es {mi_lib.factorial(5)}")
 
 print(f"La raiz entera del número ingresado es {mi_lib.raizCuadrada(144)")
 
