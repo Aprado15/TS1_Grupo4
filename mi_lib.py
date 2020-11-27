190772d93e2b11f25960a4cf0f0f21d0b4b9afc5
DiegoPantaU <74800768+DiegoPantaU@users.noreply.github.com>
2020-11-27 16:33:59 -0500

    Update mi_lib.py

--- mi_lib.py
+++ mi_lib.py
@@ -35,10 +35,5 @@ def esCapicua(numi):
             return True
   
   
-        def AAAAAAAAAAAAAAA(numi):
-    if numi >= 0:
-        if str(numi) == str(numi)[::-1]:
-            return False
-        else:
-            return True
+      
 
