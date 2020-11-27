c2805ea6714c766909823245f0ac38d71fe95da7
Aprado15 <74795462+Aprado15@users.noreply.github.com>
2020-11-27 17:10:04 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@ import mi_lib
 
 print(f"El número es primo: {mi_lib.esPrimo(3)}")
 
-print(f"El factorial del número es: {mi_lib.esFactorial(5)}")
+print(f"El factorial del número es: {mi_lib.esFactorial(3)}")
 
 print(f"El número tiene raíz entera: {mi_lib.esRaiz(64)}")
 
