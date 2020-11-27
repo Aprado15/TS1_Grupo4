0e1e736c1749f60a3a75ed3691ff0d2d2da772ec
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-27 15:16:13 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,8 +1,9 @@
 import mi_lib
-print(esPrimo(2))
 
-print(esFactorial(5))
+print(f"El número es primo: {mi_lib.esPrimo(2)}")
 
-print(esRaiz(36))
+print(f"El factorial del número es: {mi_lib.esFactorial(5)}")
 
-print(esCapicua(10107))
+print(f"El número tiene raíz entera: {mi_lib.esRaiz(36)}")
+
+print(f"El número es capicúa: {mi_lib.esCapicua(10107)}")
