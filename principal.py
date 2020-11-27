4d2577fad9daeb7c010a07dc7ad6c87da98c8c7a
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-26 23:14:39 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,2 +1,8 @@
 import mi_lib
-print(f"Si sale true El número ingresado es primo y si sale false no es primo{"mi_lib.esPrimo(2)}")
+print(f"Si sale true El número ingresado es primo y si sale false no es primo{"mi_lib.esPrimo(7)}")
+
+print(f"El factorial del número ingresado es{"mi_lib.esFactorial(5)}")
+
+print(f"La raiz entera del número ingresado es{"mi_lib.esRaiz(144)")
+
+print(f"Los números capicúas son{"mi_lib.esCapicua")
