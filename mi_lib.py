42ee9508357192dd11c524892f248a75d84487c0
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-27 12:16:34 -0500

    Update mi_lib.py

--- mi_lib.py
+++ mi_lib.py
@@ -1,40 +1,35 @@
 def esPrimo(num):
-    if num == 1 or num ==0:
+    if num < 2:
         return False
-    elif num == 2:
-        return True
-    elif num >2:
-        for i in range(2, num):
-            if num % i == 0:
-                return False
-            elif num % i !=0 and i==num-1:
-                return True 
-
-def factorial (*fac):
-    for x in fac:
-        fact=1
-        for y in range(1,x+1):
-            fact=fact*y
-        print fact
-
-def esCapicua(cap):
-#Intervalo inferior
-a=10
-#Intervalo superior 
-b=100
-#Contador para la cantidad de capicuas
-c=0
-print("Los capicuas encontrados son los siguientes:")
-for i in range(a,b+1):
-    num_s=str(i)
-    num_list=list(num_s)
-    if num_list==num_list[::-1]:
-        cap=''.join(num_list)
-        c+=1
-        print(cap)
-        
-print("Total de Capicuas:",c)
+    for i in range(2,num):
+        if num % i ==0:
+            return False
+    return True
 
 
-def raizCuadrada(n):
-    print (n**.5)
+
+def esFactorial(n):
+    f=1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+
+def esRaiz(nume):
+    x= nume/2
+    for i in range (20):
+        if x * x == nume:
+            return True
+        else:
+            x= (x + (nume/x))/2
+    return False
+
+
+
+def esCapicua(numi):
+    if numi >= 0:
+        if str(numi) == str(numi)[::-1]:
+            return True
+        else:
+            return False
