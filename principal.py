7de7be902f68807fcf9f339b103fd5b00e0cc4e5
Aprado15 <74795462+Aprado15@users.noreply.github.com>
2020-11-27 17:28:02 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,9 +1,12 @@
 import mi_lib
-
-print(f"El número es primo: {mi_lib.esPrimo(2)}")
-
+print(f"Importando de la libreria la primera funcion")
+print(f"El numero que elegimos es primo: {mi_lib.esPrimo(2)}")
+---------------------------------------
+print(f"Importando de la libreria la segunda funcion")
 print(f"El factorial del número es: {mi_lib.esFactorial(5)}")
-
+--------------------------------------
+print(f"Importando de la libreria la tercera funcion")
 print(f"El número tiene raíz entera: {mi_lib.esRaiz(36)}")
-
+--------------------------------------
+print(f"Importando de la libreria la cuarta funcion")
 print(f"El número es capicúa: {mi_lib.esCapicua(10107)}")
