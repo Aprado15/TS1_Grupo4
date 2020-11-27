b59a3263bf36481e1a5c740df2984f362538a7d4
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-27 01:45:12 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,4 +1,9 @@
 import mi_lib
 print(f"El numero es primo {mi_lib.esPrimo(7)}")
 
+print(f"El factorial del número ingresado es {mi_lib.factorial(5)}")
+
+print(f"La raiz entera del número ingresado es {mi_lib.raizCuadrada(144)")
+
+print(f"Los números capicúas son {mi_lib.esCapicua}")
 
