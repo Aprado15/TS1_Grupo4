15a383ed9a03423a1cd8c985da6bf06d39e46218
Juansuper1621 <66015438+Juansuper1621@users.noreply.github.com>
2020-11-27 16:36:06 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@ import mi_lib
 
 print(f"El número es primo: {mi_lib.esPrimo(2)}")213
 
-print(f"El factorial del número es: {mi_lib.esFactorial(5)}")122
+print(f"El factorial del número es: {mi_lib.esFactorial(5)}")12
 
 print(f"El número tiene raíz entera: {mi_lib.esRaiz(36)}")23ssa221
 1
