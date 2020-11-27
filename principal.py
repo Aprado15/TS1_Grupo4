ae5555aed6d24ce79967ed3d5902bf09c93bbccc
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-27 01:33:07 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,6 +3,6 @@ print(f"Si sale true El número ingresado es primo y si sale false no es primo",
 
 print(f"El factorial del número ingresado es {mi_lib.factorial(5)}")
 
-print(f"La raiz entera del número ingresado es {mi_lib.raizCuadrada(144)")
+
 
 print(f"Los números capicúas son {mi_lib.esCapicua}")
