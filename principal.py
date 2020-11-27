559c9b331fe0024e32f86964b09308210f6a5ab8
Aprado15 <74795462+Aprado15@users.noreply.github.com>
2020-11-27 17:49:47 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,9 +2,15 @@ import mi_lib
 print(f"Importando de la libreria la primera funcion")
 print(f"El numero que elegimos es primo: {mi_lib.esPrimo(2)}")
 ---------------------------------------
+print(f"Otra forma de hallar el numero primo usando la funcion evaluarPrimo")
+print(f"El numero que elegimos es primo: {mi_lib.evaluarPrimo(2)}")
+-----------------------------------------
 print(f"Importando de la libreria la segunda funcion")
 print(f"El factorial del número es: {mi_lib.esFactorial(5)}")
 --------------------------------------
+print(f"Otra forma de hallar el factorial de un numero usando la funcion esFactoriales")
+print(f"El factorial del número es: {mi_lib.esFactoriales(5)}"
+      -------------------------------
 print(f"Importando de la libreria la tercera funcion")
 print(f"El número tiene raíz entera: {mi_lib.esRaiz(36)}")
 --------------------------------------
