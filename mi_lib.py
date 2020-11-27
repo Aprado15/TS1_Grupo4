5490fe8cedbfb49a734df6bcb0df8177ebc1f0f6
francohoyos1810 <74795523+francohoyos1810@users.noreply.github.com>
2020-11-26 23:22:12 -0500

    Update mi_lib.py

--- mi_lib.py
+++ mi_lib.py
@@ -9,4 +9,34 @@ def esPrimo(num):
                 return False
             elif num % divisor !=0 and i==num-1:
                 return True            
-print esPrimo(1)
+
+def factorial (*fac):
+    for x in fac:
+        fact=1
+        for y in range(1,x+1):
+            fact=fact*y
+        print fact
+
+def esCapicua(cap):
+#Intervalo inferior
+a=10
+#Intervalo superior 
+b=100
+#Contador para la cantidad de capicuas
+c=0
+print("Los capicuas encontrados son los siguientes:")
+for i in range(a,b+1):
+    num_s=str(i)
+    num_list=list(num_s)
+    if num_list==num_list[::-1]:
+        cap=''.join(num_list)
+        c+=1
+        print(cap)
+        
+print("Total de Capicuas:",c)
+
+
+def esRaiz(x): 
+    math.sqrt(x)
+    
+print(x)
