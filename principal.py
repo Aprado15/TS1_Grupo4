e582dbc3870e2f1293fe4dae8dadfdd12b96fcf6
Aprado15 <74795462+Aprado15@users.noreply.github.com>
2020-11-27 16:25:32 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,4 +6,6 @@ print(f"El factorial del número es: {mi_lib.esFactorial(5)}")
 
 print(f"El número tiene raíz entera: {mi_lib.esRaiz(36)}")
 
+print(f"El número es capicúa: {mi_lib.esCapicua(10107)}"
+
 
